9119332586152c0086a6b872c27d91c3b61775b8
davidcai <w.david.cai@gmail.com>
2014-04-26 00:03:18 -0700

    changed vendor/bower to bower/

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express')
+  , logger = require('morgan')
+  , bodyParser = require('body-parser');
+
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(logger());
+app.use(bodyParser());
+app.use(express.static(__dirname + '/app'));
+
+
+app.get('*', function(req, res) {
+  res.render('index');
+});
+
+app.listen(app.get('port'));
+console.log('Listening on port ' + app.get('port') + '...');
