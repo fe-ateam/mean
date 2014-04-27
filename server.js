7d39ee1a810f84e0cf45abed65fa07710def7a79
davidcai <w.david.cai@gmail.com>
2014-04-26 18:29:42 -0700

    clean up

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 var express = require('express')
   , logger = require('morgan')
-  , bodyParser = require('body-parser');
-
-//var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+  , bodyParser = require('body-parser')
+  , methodOverride = require('method-override')
+  , errorHandler = require('errorhandler');
 
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
-app.use(logger('dev'));
-app.use(bodyParser());
 app.use('/lib', express.static(__dirname + '/lib'));
 app.use(express.static(__dirname + '/public'));
+app.use(logger('dev'));
+app.use(bodyParser());
+app.use(methodOverride());
+app.use(errorHandler({ dumpExceptions: true, showStack: true }));
 
-
-app.get('*', function(req, res) {
-  res.render('public/index.html');
+app.get('/test', function(req, res) {
+  res.send('This is a test');
 });
 
 app.listen(app.get('port'));
